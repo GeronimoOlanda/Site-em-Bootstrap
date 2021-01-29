5492e656f52c5d77588e3a3c361f394588b42078
GeronimoOlanda <51466744+GeronimoOlanda@users.noreply.github.com>
2021-01-29 01:04:00 -0300

    Adicionando Darkmode ao site

---
+++ Assets/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#btn-1").click(function(){
+        $("body").css("background-color","#121212");
+       $(".color-white").css("color","white");
+    })
+    $("#btn-2").click(function(){
+        $("body").css("background-color","white");
+       $(".color-white").css("color","black");
+    })
+    
+})
