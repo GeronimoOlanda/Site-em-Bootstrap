dc919b0cfdb5ba0e43004c373f78b3c801fdbcd6
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-04-13 18:45:58 -0300

    Atualizando Contatos

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
     $("#btn-1").click(function(){
         $("body").css("background-color","#121212");
        $(".color-white").css("color","white");
-    })
+    });
     
     $("#btn-2").click(function(){
         $("body").css("background-color","white");
        $(".color-white").css("color","black");
        $(".color-white").css("font-weight", "bolder");
-    })
+    });
     
-})
+});
